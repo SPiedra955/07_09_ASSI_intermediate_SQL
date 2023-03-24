2c43aca2a6648e9450285d61923e6c5a4fa59267
Samuel Piedra <114516225+SPiedra955@users.noreply.github.com>
2023-03-24 19:20:06 +0100

    table sells,provideds added

--- Scripts/ddl.sql
+++ Scripts/ddl.sql
@@ -59,3 +59,78 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Clients` (
   PRIMARY KEY (`id_client`),
   UNIQUE INDEX `id_client_UNIQUE` (`id_client` ASC) VISIBLE)
 ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Provideds`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Provideds` (
+  `id_provided` INT NOT NULL AUTO_INCREMENT,
+  `start_date` DATE NOT NULL,
+  `finish_date` DATE NOT NULL,
+  `Books_id_book` INT NOT NULL,
+  `Clients_id_client` INT NOT NULL,
+  PRIMARY KEY (`id_provided`, `Books_id_book`, `Clients_id_client`),
+  UNIQUE INDEX `id_provided_UNIQUE` (`id_provided` ASC) VISIBLE,
+  INDEX `fk_Provideds_Books1_idx` (`Books_id_book` ASC) VISIBLE,
+  INDEX `fk_Provideds_Clients1_idx` (`Clients_id_client` ASC) VISIBLE,
+  CONSTRAINT `fk_Provideds_Books1`
+    FOREIGN KEY (`Books_id_book`)
+    REFERENCES `mydb`.`Books` (`id_book`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Provideds_Clients1`
+    FOREIGN KEY (`Clients_id_client`)
+    REFERENCES `mydb`.`Clients` (`id_client`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Employees`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Employees` (
+  `id_employee` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `direcction` VARCHAR(45) NOT NULL,
+  `mail` VARCHAR(45) NOT NULL,
+  `number` INT NOT NULL,
+  `job` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_employee`),
+  UNIQUE INDEX `id_employee_UNIQUE` (`id_employee` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Sells`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Sells` (
+  `id_sell` INT NOT NULL AUTO_INCREMENT,
+  `date_sell` DATE NOT NULL,
+  `price` DOUBLE NOT NULL,
+  `Books_id_book` INT NOT NULL,
+  `Clients_id_client` INT NOT NULL,
+  `Employees_id_employee` INT NOT NULL,
+  PRIMARY KEY (`id_sell`, `Books_id_book`, `Clients_id_client`, `Employees_id_employee`),
+  UNIQUE INDEX `id_sell_UNIQUE` (`id_sell` ASC) VISIBLE,
+  INDEX `fk_Sells_Books1_idx` (`Books_id_book` ASC) VISIBLE,
+  INDEX `fk_Sells_Clients1_idx` (`Clients_id_client` ASC) VISIBLE,
+  INDEX `fk_Sells_Employees1_idx` (`Employees_id_employee` ASC) VISIBLE,
+  CONSTRAINT `fk_Sells_Books1`
+    FOREIGN KEY (`Books_id_book`)
+    REFERENCES `mydb`.`Books` (`id_book`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Sells_Clients1`
+    FOREIGN KEY (`Clients_id_client`)
+    REFERENCES `mydb`.`Clients` (`id_client`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Sells_Employees1`
+    FOREIGN KEY (`Employees_id_employee`)
+    REFERENCES `mydb`.`Employees` (`id_employee`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
