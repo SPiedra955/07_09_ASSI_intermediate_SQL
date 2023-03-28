1645298e3ac93aaf2d670dd13c63f730dbc4299b
SPiedra955 <spiedramite@cifpfbmoll.eu>
2023-03-28 22:36:38 +0200

    dml.sql inserts for authors

---
+++ Scripts/dml.sql
@@ -0,0 +1,10 @@
+INSERT INTO authors(name, birth_date, country) VALUES('Miguel de Cervantes','1547-09-29', 'Spain');
+INSERT INTO authors(name, birth_date, country) VALUES('Antoine Saint-Exupéry','1990-06-29', 'France');
+INSERT INTO authors(name, birth_date, country) VALUES('John Boyne','1971-04-30','Ireland');
+INSERT INTO authors(name, birth_date, country) VALUES('Iñaki Jiménez Largo','1976-05-08', 'Spain');
+INSERT INTO authors(name, birth_date, country) VALUES('Jordi Sierra I Fabra','1947-06-26', 'Spain');
+INSERT INTO authors(name, birth_date, country) VALUES('Jordi Wild','1984-08-28','Spain');
+INSERT INTO authors(name, birth_date, country) VALUES('Allen Carr','1934-09-02','United Kingdom');
+INSERT INTO authors(name, birth_date, country) VALUES('Sandra Carbajal Tudón','1980-03-13','Spain');
+INSERT INTO authors(name, birth_date, country) VALUES('The Pokemon Company','1998-04-23','Japan');
+
