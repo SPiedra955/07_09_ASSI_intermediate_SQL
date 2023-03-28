a6a4fadc2b5b47bdaf81b900dea6ebbcadb3afe8
SPiedra955 <spiedramite@cifpfbmoll.eu>
2023-03-28 22:39:13 +0200

    dml.sql inserts for clients

--- Scripts/dml.sql
+++ Scripts/dml.sql
@@ -1,3 +1,5 @@
+-- Data for insert into 'authors'
+
 INSERT INTO authors(name, birth_date, country) VALUES('Miguel de Cervantes','1547-09-29', 'Spain');
 INSERT INTO authors(name, birth_date, country) VALUES('Antoine Saint-Exupéry','1990-06-29', 'France');
 INSERT INTO authors(name, birth_date, country) VALUES('John Boyne','1971-04-30','Ireland');
@@ -8,3 +10,10 @@ INSERT INTO authors(name, birth_date, country) VALUES('Allen Carr','1934-09-02',
 INSERT INTO authors(name, birth_date, country) VALUES('Sandra Carbajal Tudón','1980-03-13','Spain');
 INSERT INTO authors(name, birth_date, country) VALUES('The Pokemon Company','1998-04-23','Japan');
 
+-- Data for insert into 'clients'
+
+INSERT INTO clients(name, surname, last_name, direction, email, phone_number) VALUES('Manuel', 'Ovejero', 'Martinez', 'C/Tramuntana 18', 'manuov@gmail.com', 655789453);
+INSERT INTO clients(name, surname, last_name, direction, email, phone_number) VALUES('Neus', 'Cabrera', 'Font', 'C/Mateu Enric Llado', 'neuscabrerafont@gmail.com', 611197269);
+INSERT INTO clients(name, surname, last_name, direction, email, phone_number) VALUES('Andres', 'Bosch', 'Valero', 'C/Sant Nicolau', 'abv@gmail.com', 619062308);
+INSERT INTO clients(name, surname, last_name, direction, email, phone_number) VALUES('Abel', 'Perez', 'Lopez', 'C/Can Serinya', 'abel98@gmail.com', 680134407);
+INSERT INTO clients(name, surname, last_name, direction, email, phone_number) VALUES('Maria', 'Bonin', 'Villen', 'C/Lima', 'mbonin@gmail.com', 611197269);
